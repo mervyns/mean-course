53a9d8e6e5285ae44242c161e5c56aca8506c054
mervyns <mervyns@gmail.com>
2018-08-27 00:38:24 +0800

    added redirect to edit and views and added spinner to views

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -25,10 +25,12 @@ export class PostListComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
+    this.isLoading = true;
     this.postsService.getPosts();
     this.postsSub = this.postsService
       .getPostUpdateListener()
       .subscribe((posts: Post[]) => {
+        this.isLoading = false;
         this.posts = posts;
       });
   }
