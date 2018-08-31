ea47ce8d868d46fc53fe8750a50977f227e5ac18
mervyns <mervyns@gmail.com>
2018-08-31 17:34:54 +0800

    saving token in local storage

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -4,11 +4,12 @@ import {
   RouterStateSnapshot,
   Router
 } from "@angular/router";
-import { Injectable } from "@angular/core"
-import {Observable} from "rxjs"
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
-import { AuthService } from "./auth.service"
+import { AuthService } from "./auth.service";
 
+@Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -18,7 +19,7 @@ export class AuthGuard implements CanActivate {
   ): boolean | Observable<boolean> | Promise<boolean> {
     const isAuth = this.authService.getIsAuth();
     if (!isAuth) {
-      this.router.navigate(['/login']);
+      this.router.navigate(["/login"]);
     }
     return isAuth;
   }
