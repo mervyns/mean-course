1ea7a799d06c27af46e4a3318861bb8aa7bd1f8c
mervyns <mervyns@gmail.com>
2018-08-31 15:58:00 +0800

    added authentication headers

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,9 +5,16 @@ import { AuthData } from "./auth-data.model";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
+
   createUser(email: string, password: string) {
-    const authData: AuthData = {email: email, password: password};
+    const authData: AuthData = { email: email, password: password };
     this.http
       .post("http://localhost:3000/api/user/signup")
       .subscribe(response => {
@@ -16,10 +23,12 @@ export class AuthService {
   }
 
   login(email: string, password: string) {
-    const authData: AuthData = {email: email, password: password};
-    this.http.post("http://localhost:3000/api/user/login")
+    const authData: AuthData = { email: email, password: password };
+    this.http
+      .post<{ token: string }>("http://localhost:3000/api/user/login")
       .subscribe(response => {
-        console.log(response);
-      })
+        const token = response.token;
+        this.token = token;
+      });
   }
 }
